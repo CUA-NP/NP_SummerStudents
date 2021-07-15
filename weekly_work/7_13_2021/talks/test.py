d95eb319b41a1402ae3bd05345c006fb4c5cc7f9
Richard Trotta <trotta@cua.edu>
2021-07-15 01:20:30 -0400

    Added Richard's 4th python tutorial.

---
+++ weekly_work/7_13_2021/talks/test.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+#
+# Description:
+# ================================================================
+# Time-stamp: "2021-07-14 23:27:05 trottar"
+# ================================================================
+#
+# Author:  Richard L. Trotta III <trotta@cua.edu>
+#
+# Copyright (c) trottar
+#
+
+def runScript():
+    print("Hello...")
